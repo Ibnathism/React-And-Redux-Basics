e91f291618313b323ee5cd8b8a0ee5d274585151
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2020-03-22 12:22:51 +0600

    show a list of elements

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import Nested from './Props';
 
 class App extends Component {
+  //Outputting List
+  state = {
+    donkeys : [
+      {name: 'James', age: 12, hair: 'black', id: 1},
+      {name: 'Peter', age: 15, hair: 'red', id: 2},
+      {name: 'Swiny', age: 16, hair: 'white', id: 3}
+    ]
+  }
   render(){
     return (
-      <div className="App">
+
+      //Outputting single or multiple element having props
+      /*<div className="App">
           <h1>My first React app!</h1>
           <p>Welcome :)</p>
           <Nested name="Muntaka" course="React and Redux" session="12"/>
           <Nested name="Ashiq" course="Django" session="10"/>
+          <Nested donkeys={this.state.donkeys}/>
+      </div>*/
+
+      //Outputting a List having props
+      <div className="App">
+          <h1>My first React app!</h1>
+          <p>Welcome :)</p>
+          <Nested donkeys={this.state.donkeys}/>
       </div>
     );
   }
--- myfirstapp/src/Props.js
+++ myfirstapp/src/Props.js
@@ -3,14 +3,36 @@ import React, { Component } from 'react';
 class Nested extends Component{
     render(){
         //console.log(this.props);
-        const { name, course, session } = this.props;
+        
+        //Outputting single or multiple
+        //const { name, course, session } = this.props;
+
+        //Outputting List
+        const { donkeys } = this.props;
+        const donkeylist = donkeys.map(eachDonkey => {
+            return (
+                <div className="donkey" key={eachDonkey.id}>
+                    <div>Name: {eachDonkey.name}</div>
+                    <div>Age: {eachDonkey.age}</div>
+                    <div>Hair: {eachDonkey.hair}</div>
+                </div>
+            )
+        })
         return(
-            <div className="Nested">
+
+            //Outputting single or multiple element having props
+            /*<div className="Nested">
                 <div>Name : { name }</div>
                 <div>Course : { course }</div>
                 <div>Session : { session }</div>
+            </div>*/
+
+
+            //Outputting a List having props
+            <div className="donkey-list">
+                { donkeylist }
             </div>
-        );
+        )
     }
 }
 
